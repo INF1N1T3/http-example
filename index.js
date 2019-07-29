697d1ab1d12c361275d2aee1d1b7a3691ba79cce
dolboeb <samirich45@mail.ru>
2019-07-29 14:24:00 +0400

    wow

---
+++ index.js
@@ -0,0 +1,17 @@
+const
+	express = require('express'),
+	app = express();
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname +  '/index.html');
+});
+
+app.get('/information', (req, res) => {
+	res.sendFile(__dirname +  '/greet.html');
+});
+
+app.get('/:information/:id', (req, res) => {
+	res.send(`ID is ${req.params.information}/${req.params.id}`);
+});
+
+app.listen(3000);
