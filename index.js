3aac0ce34f0420d102d63f34acb7093327e39bef
dolboeb <samirich45@mail.ru>
2019-07-29 16:19:20 +0400

    submit

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const
 	express = require('express'),
-	app = express();
+	app = express(),
+	bodyParser = require('body-parser'),
+	urlencodedParser = bodyParser.urlencoded({extended: false});
 app.set('view engine', 'ejs');
+app.use('/public', express.static('public'));
 
-// app.get('/', (req, res) => {
-// 	res.sendFile(__dirname +  '/index.html');
-// });
+app.get('/index', (req, res) => {
+	res.render('index');
+});
+
+app.post('/index', urlencodedParser, (req, res) => {
+	if (!req.body) return res.sendStatus(400);
+	console.log(req.body);
+	res.render('index');
+});
 
 // app.get('/information', (req, res) => {
 // 	res.sendFile(__dirname +  '/greet.html');
