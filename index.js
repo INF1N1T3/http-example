6f7f21960e2a80f98a74678a7657dbd9a2a446b0
dolboeb <samirich45@mail.ru>
2019-07-29 14:33:09 +0400

    remove html and add ejs

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const
 	express = require('express'),
 	app = express();
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-	res.sendFile(__dirname +  '/index.html');
-});
+// app.get('/', (req, res) => {
+// 	res.sendFile(__dirname +  '/index.html');
+// });
 
-app.get('/information', (req, res) => {
-	res.sendFile(__dirname +  '/greet.html');
-});
+// app.get('/information', (req, res) => {
+// 	res.sendFile(__dirname +  '/greet.html');
+// });
 
-app.get('/:information/:id', (req, res) => {
-	res.send(`ID is ${req.params.information}/${req.params.id}`);
-});
+// app.get('/:information/:id', (req, res) => {
+// 	res.send(`ID is ${req.params.information}/${req.params.id}`);
+// });
 
 app.listen(3000);
